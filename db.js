e5add92e18a4e95c9e1fee1055772628b5b6814f
GeloSCV <geloscv.asc@gmail.com>
2022-12-03 16:09:56 -0400

    actualizacion db

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool= new Pool({
     user: process.env.user_db,
     password:process.env.password_db,
-    host:"localhost",
+    host:process.env.host_db,
     port: process.env.port_db,
     database:process.env.database_db,
 });
